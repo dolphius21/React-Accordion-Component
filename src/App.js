b9bb6df01fa930fdd2260890403758da5452447f
dolphius21 <johnrudolphlee@gmail.com>
2021-06-02 18:39:11 +0800

    done with accordion

--- src/App.js
+++ src/App.js
@@ -1,55 +1,133 @@
 import { useState } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { IconContext } from 'react-icons';
 import { FiChevronUp, FiChevronDown } from 'react-icons/fi';
 import questions from './data';
+import graphic from './images/UIUX_design.svg';
 
+const downAnimation1 = keyframes`
+ 0% {
+    transform: translateY(-7px);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+`;
+const downAnimation2 = keyframes`
+ 0% {
+    transform: translateY(-50px);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+`;
+const transparency = keyframes`
+ 0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+const AccordionContainer = styled.div``;
 const Card = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: space-around;
   background: white;
-  max-width: 800px;
-  padding: 2rem;
+  max-width: 1200px;
+  padding: 3rem;
   border-radius: 2rem;
+  box-shadow: 0px 0px 40px -20px rgba(5, 71, 176, 0.5);
+  animation: ${downAnimation2} 0.5s 0.1s ease-out both;
 `;
-const ImgContainer = styled.div``;
-const Heading = styled.h2``;
-const Container = styled.div``;
-const ItemsContainer = styled.div``;
-const Question = styled.div``;
-const Answer = styled.div``;
+const ItemsContainer = styled.div`
+  margin-bottom: 7px;
+  border: 1px solid #d4dae9;
+  border-radius: 5px;
+`;
+const ImgContainer = styled.div`
+  width: 600px;
+  img {
+    width: 550px;
+    padding: 0 1rem;
+    height: 100%;
+    display: block;
+  }
+`;
+const Question = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 5px;
+  padding: 1rem;
+  cursor: pointer;
+  width: 500px;
+  transition: all 0.2s linear;
+  h3 {
+    font-size: 1.1rem;
+    font-weight: 600;
+  }
+  h3:hover {
+    color: #3975fd;
+  }
+`;
+const Answer = styled.div`
+  width: 500px;
+  padding: 1rem;
+  background: #eef9ff;
+  border-radius: 5px;
+  transition: all 0.2s linear;
+  animation: ${downAnimation1} 0.3s 0.1s ease-out both;
+  font-size: 0.9rem;
+`;
 
 function Accordion() {
   const [clicked, setclicked] = useState(false);
 
   const handleClick = (index) => {
     if (clicked === index) {
-      setclicked(null);
+      return setclicked(null);
     }
 
     setclicked(index);
   };
 
   return (
-    <IconContext.Provider value={{ size: '25px' }}>
+    <IconContext.Provider value={{ size: '20px' }}>
       <Card>
-        <ImgContainer></ImgContainer>
-        <Heading>FAQ</Heading>
-        <Container>
+        <ImgContainer>
+          <img src={graphic} alt="UI/UX Graphic" />
+        </ImgContainer>
+        <AccordionContainer>
           {questions.map((item, index) => (
-            <ItemsContainer
-              key={item.id}
-              onClick={(index) => {
-                handleClick(index);
-              }}
-            >
-              <Question>
+            <ItemsContainer>
+              <Question
+                key={index}
+                onClick={() => {
+                  handleClick(index);
+                }}
+                style={{
+                  borderBottom:
+                    clicked === index ? '1px solid #d4dae9' : undefined,
+                  borderLeft:
+                    clicked === index ? '5px solid #3975fd' : undefined,
+                  color: clicked === index ? '#3975fd' : undefined
+                }}
+              >
                 <h3>{item.question}</h3>
-                <span>{clicked ? <FiChevronDown /> : <FiChevronUp />}</span>
+                <span>
+                  {clicked === index ? <FiChevronUp /> : <FiChevronDown />}
+                </span>
               </Question>
-              <Answer>{item.answer}</Answer>
+              {clicked === index ? <Answer>{item.answer}</Answer> : null}
             </ItemsContainer>
           ))}
-        </Container>
+        </AccordionContainer>
       </Card>
     </IconContext.Provider>
   );
